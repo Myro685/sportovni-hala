8fb7eb3ff0698a8fd6e761e64f99125b035d297e
JanStastny2 <stastnyj162@gmail.com>
2025-03-31 14:16:54 +0200

    trainig - v0

---
+++ src/scripts/training.js
@@ -0,0 +1,16 @@
+
+// Inicializace Supabase
+const supabaseClient = window.supabase.createClient(
+    "https://xpxurtdkmufuemamajzl.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhweHVydGRrbXVmdWVtYW1hanpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5NTk3MzksImV4cCI6MjA1NzUzNTczOX0.uRPj22s06XSTvuuHGz-7oAqfTRp2LqUFTCKxC8QprMU"
+  );
+
+
+// Načtení dat
+document.addEventListener("DOMContentLoaded", loadData);
+
+async function loadData() {
+
+    
+}
+
